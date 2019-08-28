3423409bd1127d4daf78b1345e1af35d74a9bb08
maharoz <mahruj66@gmail.com>
2019-08-28 19:50:29 +0600

    styling..

--- lib/main.dart
+++ lib/main.dart
@@ -62,16 +62,25 @@ class MyHomePage extends StatelessWidget {
                         ),
                       ),
                       padding: EdgeInsets.all(10),
-                      child: Text(tx.amount.toString(),
-                      style: TextStyle(fontWeight:FontWeight.bold,
-                      fontSize: 20,color:Colors.purple),
+                      child: Text(
+                        tx.amount.toString(),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple),
                       ),
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
-                        Text(tx.title),
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
                         Text(
                           tx.date.toString(),
+                          style: TextStyle(color:Colors.grey),
                         )
                       ],
                     )
