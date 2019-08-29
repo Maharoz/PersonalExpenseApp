88a4f73d930e5df74a664f68e96ef73b8f0394eb
maharoz <mahruj66@gmail.com>
2019-08-29 21:43:07 +0600

    toolbar button and floating button added

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,11 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.add),onPressed: (){
+            
+          },)
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -45,6 +50,11 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(child: Icon(Icons.add),
+      onPressed: (){
+
+      },),
     );
   }
 }
