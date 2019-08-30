2b318e19ef12a4f4706c01c5bdcb0e042847afb2
maharoz <mahruj66@gmail.com>
2019-08-30 23:41:45 +0600

    update

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,7 +12,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 450,
       child: transactions.isEmpty?Column(children: <Widget>[
         Text('No Transaction added yet!!',style:Theme.of(context).textTheme.title,
         ),
