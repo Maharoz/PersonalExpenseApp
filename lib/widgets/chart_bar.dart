f89fb202ba4ad49d52a82f124f71f1e34c436e04
maharoz <mahruj66@gmail.com>
2019-08-30 20:48:07 +0600

    Finishing the chart bar

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -10,7 +10,9 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        FittedBox(
+        child:Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        ),
         SizedBox(
           height: 4,
         ),
@@ -30,7 +32,10 @@ class ChartBar extends StatelessWidget {
                 
                   heightFactor: spendingPctOfTotal,
                   child: Container(
-                    decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10),
+                      ),
                   ))
             ],
           ),
