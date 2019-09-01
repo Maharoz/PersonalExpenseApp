c2842936f0b29a60b4df8915f3909b1195981a6e
maharoz <mahruj66@gmail.com>
2019-09-01 22:07:30 +0600

    Using device size in condition

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,23 +12,22 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? LayoutBuilder(builder:(ctx,constraints){
-          return Column(
-            children: <Widget>[
-              Text(
-                'No Transaction added yet!!',
-                style: Theme.of(context).textTheme.title,
-              ),
-              SizedBox(height: 10),
-              Container(
-                height: constraints.maxHeight *0.6,
-                child:
-                    Image.asset('assets/images/waiting.png', fit: BoxFit.cover),
-              )
-            ],
-          );
-
-        }) 
+        ? LayoutBuilder(builder: (ctx, constraints) {
+            return Column(
+              children: <Widget>[
+                Text(
+                  'No Transaction added yet!!',
+                  style: Theme.of(context).textTheme.title,
+                ),
+                SizedBox(height: 10),
+                Container(
+                  height: constraints.maxHeight * 0.6,
+                  child: Image.asset('assets/images/waiting.png',
+                      fit: BoxFit.cover),
+                )
+              ],
+            );
+          })
         : ListView.builder(
             itemBuilder: (ctx, index) {
               return Card(
@@ -47,7 +46,13 @@ class TransactionList extends StatelessWidget {
                       style: Theme.of(context).textTheme.title),
                   subtitle:
                       Text(DateFormat.yMMMd().format(transactions[index].date)),
-                  trailing: IconButton(
+                  trailing: MediaQuery.of(context).size.width > 460
+                   ? FlatButton.icon(
+                     icon:  Icon(Icons.delete),
+                     label : Text('Delete'),
+                    color:Theme.of(context).errorColor,
+                    onPressed: () => deleteTx(transactions[index].id),)
+                    : IconButton(
                     icon: Icon(Icons.delete),
                     color: Theme.of(context).errorColor,
                     onPressed: () => deleteTx(transactions[index].id),
